47cac46fe7f2c484b88ecf2ff88ac29fe5af36d4
abirmaji108-glitch <abirmaji108@gmail.com>
2026-02-13 08:25:44 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -423,7 +423,7 @@ const adminLimiter = rateLimit({
 // CORS headers
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
